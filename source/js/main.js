5d31eb1b2f52cd16a8ed7bf6a23686993b59ede3
Toma Kazanskaya <tkazanskaya888@gmail.com>
2024-04-22 22:58:25 +0300

    started tours

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,6 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
 
 import { initMenu } from './modules/menu.js';
+import './modules/swiper.js';
 
 initMenu();
---
+++ source/js/modules/swiper.js
@@ -0,0 +1,60 @@
+import 'swiper/css';
+import Swiper from 'swiper';
+import {Navigation, Pagination} from 'swiper/modules';
+//import 'swiper/css/pagination';
+
+export const heroSwiper = new Swiper('.swiper--hero', {
+  direction: 'horizontal',
+  loop: true,
+  modules: [ Pagination ],
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:  true,
+    bulletElement: 'button',
+  },
+
+  watchSlidesProgress: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+});
+
+// heroSwiper.on('slideChange', function () {
+
+// })
+
+
+export const toursSwiper = new Swiper('.swiper--tours', {
+  direction: 'horizontal',
+  loop: true,
+  modules: [ Navigation ],
+
+  navigation: {
+    prevEl: '.swiper-button--prev',
+    nextEl: '.swiper-button--next',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      initialSlide: 0,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+
+    1366: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
+});
+
+
+
