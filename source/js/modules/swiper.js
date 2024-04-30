e059d2e3579e15b332696ab75ae89a25e9cc46d0
Toma Kazanskaya <tkazanskaya888@gmail.com>
2024-05-01 00:03:53 +0300

    adds reviews

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -49,7 +49,7 @@ export const toursSwiper = new Swiper('.swiper--tours', {
       spaceBetween: 18,
     },
 
-    1366: {
+    1440: {
       slidesPerView: 3,
       spaceBetween: 30,
     }
@@ -80,9 +80,49 @@ export const instructorsSwiper = new Swiper('.swiper--instructors', {
       initialSlide: 0,
     },
 
-    1366: {
+    1440: {
       slidesPerView: 4,
       spaceBetween: 20,
     }
+  }
+});
+
+export const reviewsSwiper = new Swiper('.swiper--reviews', {
+  direction: 'horizontal',
+  loop: false,
+  modules: [ Navigation ],
+  slidesPerView: 'auto',
+  //slideShadows: true,
+  navigation: {
+    prevEl: '.swiper-buttons__reviews-prev',
+    nextEl: '.swiper-buttons__reviews-next',
+  },
+//  effect: 'coverFlow',
+//   coverflowEffect: {
+//     slideShadows: true,
+//     rotate: 30,
+//   },
+
+  breakpoints: {
+    320: {
+      width: 290,
+      slidesPerView: 1,
+      spaceBetween: 40,
+      initialSlide: 0,
+    },
+
+    768: {
+      width: 565,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      initialSlide: 0,
+    },
+
+    1440: {
+      width: 700,
+      slidesPerView: 1,
+      spaceBetween: 120,
+      initialSlide: 0,
+    },
   }
 });
