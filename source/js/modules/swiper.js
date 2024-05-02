61c9cc018167dc6252da7e4c5c356c2694e168b0
Toma Kazanskaya <tkazanskaya888@gmail.com>
2024-05-03 00:47:19 +0300

    adds footer

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -126,3 +126,118 @@ export const reviewsSwiper = new Swiper('.swiper--reviews', {
     },
   }
 });
+
+
+//const breakpoint = window.matchMedia('(min-width: 1440px)');
+
+// export const advSwiper = new Swiper('.swiper--advantages', {
+//   direction: 'horizontal',
+//   loop: false,
+//   modules: [ Navigation ],
+//   slidesPerView: 'auto',
+//   navigation: {
+//     prevEl: '.swiper-buttons__advantages-prev',
+//     nextEl: '.swiper-buttons__advantages-next',
+//   },
+
+//   on: {
+//     resize: function resizesScreen(swiper) {
+//       let breakpoint = window.innerWidth;
+//       if (breakpoint < 1440) {
+//         swiper.disable();
+//         swiper.destroy(true, true);
+//       } else {
+//         swiper.enable();
+//         swiper.init();
+//       }
+
+//       if (breakpoint >= 1440) {
+
+//         swiper.enable();
+//         swiper.init();
+//       } else {
+//          swiper.disable();
+//         swiper.destroy(true, true);
+//       }
+//     }
+//   }
+// });
+
+// if (window.innerWidth < 1440) {
+//   advSwiper.destroy();
+// }
+
+// function resizesScreen() {
+
+//   if (breakpoint.matches) {
+
+//    let advSwiper = new Swiper('.swiper--advantages', {
+//       direction: 'horizontal',
+//     loop: false,
+//   modules: [ Navigation ],
+//   slidesPerView: 'auto',
+//   navigation: {
+//     prevEl: '.swiper-buttons__advantages-prev',
+//     nextEl: '.swiper-buttons__advantages-next',
+//   },
+
+
+//     });
+//   } else {
+//     Swiper.destroy(true, true);
+//   }
+// }
+// breakpoint.addEventListener('change', resizesScreen)
+
+const swiperList = document.querySelector('.advantages__swiper');
+const sliderList = document.querySelector('.advantages__list');
+const sliderItems = Array.from(sliderList.querySelectorAll('.advantages__item'));
+
+const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
+  let swiper;
+
+  breakpoint = window.matchMedia(breakpoint);
+
+  const enableSwiper = function (className, settings) {
+    swiper = new Swiper(className, settings);
+  };
+
+  const checker = function() {
+    swiperList.classList.remove('swiper');
+    sliderList.classList.remove('swiper-wrapper');
+    sliderItems.forEach((item) => {
+      item.classList.remove('swiper-slide');
+    })
+
+    if (breakpoint.matches) {
+      swiperList.classList.add('swiper');
+      sliderList.classList.add('swiper-wrapper');
+      sliderItems.forEach((item) => {
+        item.classList.add('swiper-slide');
+      })
+
+      return enableSwiper(swiperClass, swiperSettings);
+    } else {
+      if (swiper !== undefined) {
+        swiper.destroy(true,true);
+        swiper.disable();
+      }
+      //return;
+    }
+  };
+
+  breakpoint.addEventListener('change', checker);
+  checker();
+};
+
+
+resizableSwiper('(min-width: 1440px)', '.swiper--advantages', {
+  direction: 'horizontal',
+  loop: true,
+  modules: [ Navigation ],
+  slidesPerView: 'auto',
+  navigation: {
+    prevEl: '.swiper-buttons__advantages-prev',
+    nextEl: '.swiper-buttons__advantages-next',
+  }
+});
